a81cea48f94a6f95fc31c284ce705c54e5102395
Server Acim <server.acim@yandex.com>
2015-11-25 23:53:10 +0200

    just leanpub is left

--- 2-SOLFEJ_SINAVLAR/1-Vize/Dikte/Derleme_point_and_clicksiz_PDF.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-import os
-a = "/home/sacim/LilyPond_Frescobaldi/lilypond_executive/bin/lilypond -dno-point-and-click *.ly"
-os.system(a)
