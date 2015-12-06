0da5f01cb041a5f80108bac60d76884d7f92d8b9
Server Acim <server.acim@yandex.com>
2015-12-06 20:33:10 +0200

    Bazı degisiklikler ve eklemeler deneme amacli yapildi.

--- manuscript/kaynak_dosyalar/Derleme_PNG.py
+++ manuscript/kaynak_dosyalar/Derleme_PNG.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 import os
-a = "/home/sacim/LilyPond_Frescobaldi/lilypond_executive/lilypond/usr/bin/lilypond -dbackend=eps -dno-gs-load-fonts -dinclude-eps-fonts -dresolution=500 --ps --png *.ly"
+a = "/home/sacim/LilyPond_Frescobaldi/lilypond_executive/lilypond/usr/bin/lilypond -dbackend=eps -dno-gs-load-fonts -dinclude-eps-fonts -dresolution=350 --ps --png *.ly"
 os.system(a)
